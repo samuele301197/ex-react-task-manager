a28b2ef43c626657b51c5bc504a2028536eb1dfd
samuele301197 <parisisamu32@gmail.com>
2025-09-25 18:20:05 +0200

    feat:milestone-2

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TaskList from "./TaskList";
 import AddTask from "./AddTask";
 import NavBar from "./NavBar";
+import GlobalProvider from "./context/GlobalContext.jsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/add" element={<AddTask />} />
-        <Route path="/list" element={<TaskList />} />
-      </Routes>
+      <GlobalProvider>
+        <NavBar />
+        <Routes>
+          <Route path="/add" element={<AddTask />} />
+          <Route path="/tasks" element={<TaskList />} />
+          <Route path="*" element={<h1>Pagina non trovata</h1>} />
+        </Routes>
+      </GlobalProvider>
     </BrowserRouter>
   );
 }
---
+++ src/context/GlobalContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useEffect, useState } from "react";
+
+const GlobalContext = createContext();
+
+export default function GlobalProvider({ children }) {
+  const [tasks, setTasks] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const res = await fetch(`${import.meta.env.VITE_API_URL}/tasks`);
+        if (!res.ok) throw new Error("Errore nel fetch dei tasks");
+        const data = await res.json();
+        console.log("Dati ricevuti dall'API:", data);
+        setTasks(data);
+      } catch (err) {
+        console.error("Errore", err);
+        setError(err);
+      }
+    };
+    fetchTasks();
+  }, []);
+
+  const addTask = (task) => setTasks((prev) => [...prev, task]);
+
+  return (
+    <GlobalContext.Provider value={{ tasks, addTask, error }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+}
